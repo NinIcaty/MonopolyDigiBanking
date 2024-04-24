186a3386357093938bb09bacb573606c08e4c1ea
NinIcaty <136358167+NinIcaty@users.noreply.github.com>
2024-04-24 15:59:37 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ def transfer():
     p1m, p2m, p3m, p4m , p5m, p6m, p7m, p8m , bank = startMoney, startMoney, startMoney, startMoney, startMoney, startMoney, startMoney, startMoney, 25580
 
     while True:
-        print("player 1 has",p1m,"player 2 has", p2m,"player 3 has", p3m, "player 4 has",p4m,"player 5 has", p5m ,"player 6 has", p6m , "player 7 has",p7m , "player 8 has",p8m ,"Money in the bank" + bank)
+        print("player 1 has",p1m,"player 2 has", p2m,"player 3 has", p3m, "player 4 has",p4m,"player 5 has", p5m ,"player 6 has", p6m , "player 7 has",p7m , "player 8 has",p8m ,"Money in the bank" , bank)
         try:
             transferFrom = int(input("Transfer from? (1-8)9 is bank\n"))
             transferTo = int(input("Transfer to? (1-8)9 is bank \n"))
